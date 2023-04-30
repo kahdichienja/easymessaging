c7f8344f55b511a67b756af9460b3547368be899
kahdichienja <agooclinton@gmail.com>
2023-04-30 21:15:49 +0300

    groupMessages, getUserGroups,createMessage, addUserToGroup

--- app/Http/Controllers/Api/MessagesController.php
+++ app/Http/Controllers/Api/MessagesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Models\User;
 use App\Models\Group;
 use App\Models\Message;
 use App\Models\GroupUser;
@@ -52,6 +53,9 @@ class MessagesController extends Controller
     {
         $user = Auth::user();
 
+        $groupId = $request->group_id;
+        $userId = $request->user_id;
+
         // Validate request data
         $validatedData = $request->validate([
             'group_id' => 'required|integer',
@@ -60,26 +64,43 @@ class MessagesController extends Controller
 
 
         // Check if user is the owner of the group
-        // $group = Group::find($validatedData['group_id']);
 
-        // if (!$group || $group->group_id !== $user->id) {
-        //     return response()->json([
-        //         'message' => 'You do not have permission to add members to this group'
-        //     ], 403);
-        // }
+        $groupUser = GroupUser::where('group_id', $groupId)->with('user')->first();
+
+        if (!$groupUser || !$groupUser->is_admin || $groupUser->user_id !== $user->id) {
+
+            return $this->error('You do not have permission to add members to this group', 403);
+        }
 
 
         // Check if user to be added exists
+        $userexists = User::where('id', $userId)->exists();
+
+        if (!$userexists) {
+            return $this->error('User not in the system', 403);
+        }
+
         // Check if user is already a member of the group
+        $userAlreadyInGroup = GroupUser::where('user_id', $userId)
+        ->where('group_id', $groupId)
+        ->exists();
 
-        // Create group user
-        $groupUser = new GroupUser;
-        $groupUser->group_id = $validatedData['group_id'];
-        $groupUser->user_id = $validatedData['user_id'];
-        $groupUser->is_admin = $request->is_admin??false;
-        $groupUser->save();
+        if ($userAlreadyInGroup) {
+            return $this->error('user is already a member of the group', 403);
+        }else {
+            // Create group user
+            $newuser = new GroupUser;
+            $newuser->group_id = $groupId;
+            $newuser->user_id = $userId;
+            $newuser->is_admin = $request->is_admin??false;
+            $newuser->save();
 
-        return $this->success($groupUser, "{$groupUser->user->name} added to {$groupUser->group->name} successfully");
+            // broadcast new user added to the group
+
+            return $this->success($groupUser, "{$groupUser->user->name} added to {$groupUser->group->name} successfully");
+        }
+
+        return $this->success($groupUser, " successfully");
 
     }
     public function createMessage(Request $request): JsonResponse
@@ -96,9 +117,7 @@ class MessagesController extends Controller
         // Check if user is a member of the group
         $group = Group::find($validatedData['group_id']);
         if (!$group || !$group->users()->where('users.id', $user->id)->exists()) {
-            return response()->json([
-                'message' => 'You are not a member of this group'
-            ], 403);
+            return $this->error('You are not a member of this group', 403);
         }
 
         // Create the message
@@ -132,12 +151,23 @@ class MessagesController extends Controller
         $userId = Auth::id();
 
         //  Get the authenticated user's groups
-        $groups = Group::whereHas('users', function ($query) use ($userId) {
-            $query->where('user_id', $userId);
-        })
-        ->with('messages', function($query) {
-            $query->latest('created_at')->with('user')->first();
-        })
+        // $groups = Group::whereHas('users', function ($query) use ($userId) {
+        //     $query->where('user_id', $userId);
+        // })
+        // ->with('messages', function($query) {
+        //     $query->latest('created_at')->with('user')->first();
+        // })
+        // ->get();
+        $groups = GroupUser::where('user_id', $userId)
+        ->with([
+            'group' => function ($query) {
+                $query->with([
+                    'messages' => function ($query) {
+                        $query->latest()->first();
+                    }
+                ]);
+            }
+        ])
         ->get();
 
 
@@ -147,18 +177,39 @@ class MessagesController extends Controller
     public function groupMessages(Request $request): JsonResponse
     {
         $userId = Auth::id();
-        $messages = Group::with([
-            'messages' => function ($query) {
-                $query->with('user')->orderBy('created_at', 'desc');
-            },
-            'users' => function ($query) use ($userId) {
-                $query->where('user_id', '<>', $userId);
-            }
-        ])
+        // Get the group ID from the request
+        $groupId = $request->group_id;
+
+        $messages = Group::where('id', $request->group_id)
         ->whereHas('users', function ($query) use ($userId) {
             $query->where('user_id', $userId);
         })
-        ->findOrFail($request->group_id);
-        return $this->success($messages);
+        ->with(['messages' => function ($query) {
+            $query->with('user')->orderBy('created_at', 'DESC');
+        }])
+        ->first();
+
+        // Check if the authenticated user belongs to the group
+        $userInGroup = GroupUser::where('group_id', $groupId)->where('user_id', $userId)->exists();
+        // Get the authenticated user's membership in the group
+
+        if ($userInGroup) {
+            // Get the group and its users
+            $groupUsers = GroupUser::where('group_id', $groupId)->with('user')->get();
+
+            // Return the users in the group
+            // return $group->users;
+
+            $data = [
+                "messages" => $messages,
+                "users" => $groupUsers,
+            ];
+
+            return $this->success($data);
+        } else {
+            // Return an error response
+            return $this->error('Unauthorized', 401);
+        }
+
     }
 }
